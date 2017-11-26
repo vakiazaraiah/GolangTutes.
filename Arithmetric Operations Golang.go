3d76ec4560e78a4833983e44b641cf692ce19f1f
azaraiahvaki <azaraiah.vaki@my.jcu.edu.au>
2017-11-26 14:16:36 +1000

    New changes

--- Arithmetric Operations Golang.go
+++ Arithmetric Operations Golang.go
@@ -1,1 +1,13 @@
 package main
+
+import "fmt"
+
+//Arithmetric Operations(/, *, -, +, %)
+
+func main()  {
+	fmt.Println("20 + 2 =", 20 + 2)
+	fmt.Println("20 * 2 =", 20 * 2)
+	fmt.Println("20 / 2 =", 20 / 2)
+	fmt.Println("20 - 2 =", 20 - 2)
+	fmt.Println("20 % 2 =", 20 % 2)
+}
